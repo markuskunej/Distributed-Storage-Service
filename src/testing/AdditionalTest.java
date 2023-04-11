23861d64470b547284f590cb40f64b088a2d5bad
LjutaPaprika <50626540+LjutaPaprika@users.noreply.github.com>
2023-04-11 17:44:19 -0400

    Added 10 additional tests for cryptography system

--- src/testing/AdditionalTest.java
+++ src/testing/AdditionalTest.java
@@ -1,31 +1,21 @@
 package testing;
 
-import java.beans.Transient;
-import java.io.File;
-import java.util.TreeMap;
-
 import app_kvServer.KVServer;
 import client.KVStore;
-import shared.messages.IKVMessage;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import static org.junit.Assert.assertTrue;
+
 import shared.messages.KVMessage;
 import shared.messages.IKVMessage.StatusType;
 
-import app_kvECS.ECSClient;
-
-import shared.messages.IECSMessage;
-import shared.messages.ECSMessage;
-
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import ecs.KVServerConnection;
-
-import java.util.TreeSet;
-
-import javax.accessibility.AccessibleHypertext;
-
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
 
 public class AdditionalTest extends TestCase {
 
@@ -34,7 +24,6 @@ public class AdditionalTest extends TestCase {
 	KVServer kvServer3;
 	KVServer kvServer4;
 	KVServer kvServer5;
-	KVServer kvServer6;
 
 	KVStore kvClient1;
 	KVStore kvClient2;
@@ -84,47 +73,71 @@ public class AdditionalTest extends TestCase {
 
 	}
 
-	// TODO add your test cases, at least 3
+	PrivateKey privateKey;
+	PublicKey publicKey;
+
 	@Test
-	public void testServerHash() {
-		KVMessage response = null;
-		Exception ex = null;
-		String hash = "";
-		// obtained by putting 127.0.0.1:7552 in md5 generator online	
-		String expected_hash = "8b05321465768a5d2f3df532d00961d9";
+	public void keyGenTest() {
+		// Test that public and private keys are generated
+		// First by a storage server, then by a client
+		KeyPairGenerator kpg = null;
 		try {
-			hash = kvServer1.getHash();
-		} catch (Exception e) {
-			ex = e;
-		}
-		assertTrue(ex == null && hash.equals(expected_hash));
+			kpg = KeyPairGenerator.getInstance("RSA/ECB/PKCS1Padding");
+		} catch (GeneralSecurityException e) {
+			throw new RuntimeException(e);
+		}	
+
+		KeyPair keyPair = kpg.generateKeyPair();
+		PrivateKey privateKey = keyPair.getPrivate();
+		PublicKey publicKey = keyPair.getPublic();
+
+		assertTrue(publicKey != null);
+		assertTrue(privateKey != null);
+		return;
 	}
 
 	@Test
-	public void testClientHash() {
-		KVMessage response = null;
-		Exception ex = null;
-		String hash = "";
-		// obtained by putting test in md5 generator online	
-		String expected_hash = "098f6bcd4621d373cade4e832627b4f6";
-		try {
-			//hash = kvClient1.hash("test");
-		} catch (Exception e) {
-			ex = e;
-		}
-		assertTrue(ex == null && hash.equals(expected_hash));
+	public void keyMatchTest() {
+		// private and public keys must not be the same
+		assertTrue(privateKey != publicKey);
+	}
+
+	byte[] temp;
+	String test_phrase = "this is a sample test phrase";
+
+	@Test
+	public void encryptionTest() {
+		// Create public and private keys
+		keyGenTest();
+		// convert test string to bytes
+		byte[] test_bytes = test_phrase.getBytes();
+		byte[] encrypted_test = kvClient1.encrypt(test_bytes, publicKey);
+		// store encrypted array in temp - used for next test
+		temp = encrypted_test;
+		// compare both byte arrays - should be different
+		assertTrue(test_bytes != encrypted_test);
 	}
 
 	@Test
-	public void testServerToServerConnection() {
-		KVMessage response = null;
+	public void decryptionTest() {
+		// Can use temp array as encrypted bytes
+		byte[] decrypted_test = kvClient1.decrypt(temp, privateKey);
+		// Convert bytes to string
+		String test = new String(decrypted_test);
+		// compare test and test_phrase, should be the same
+		assertTrue(test == test_phrase);
+	}
+
+	@Test
+	public void sendMessageTest() {
+		// check if sendMessage runs properly
 		Exception ex = null;
+
+		byte[] test_bytes = test_phrase.getBytes();
 		
 		try {
-			//connect kvServer1 to kvServer2
-			kvServer1.connectToServer("localhost", 7552);
-			//close external server connection
-			//kvServer1.closeServerConnection();
+			KVMessage msg = new KVMessage(test_bytes);
+			kvClient1.sendMessage(msg);
 		} catch (Exception e) {
 			ex = e;
 		}
@@ -132,144 +145,78 @@ public class AdditionalTest extends TestCase {
 	}
 
 	@Test
-	public void testAddKVServer() {
+	public void receiveMessageTest() {
+		// check if sendMessage runs properly
 		Exception ex = null;
+		Boolean enc = true;
 
+		byte[] test_bytes = test_phrase.getBytes();
+		
 		try {
-			kvServer6 = new KVServer("localhost:7551", "localhost", 7557, 1, null, "test_data");
-			kvServer6.start();
+			KVMessage msg = new KVMessage(test_bytes);
+			kvClient1.sendMessage(msg);
+			kvClient1.receiveMessage(enc);
 		} catch (Exception e) {
 			ex = e;
 		}
-		// assert no error, the new metadata is different from the old one, and kvServer 1 is no longer in the metadata
 		assertTrue(ex == null);
 	}
 
 	@Test
-	public void testRemoveKVServer() {
+	public void publicKeyExchangeTest() {
+		// kvClient1 (for example) is a connected client, so kvServer1 should have kvClient1's public key
+		PublicKey kvServerTest = kvServer1.getClientPublicKey();
+		PublicKey kvClientTest = kvClient1.getClientPublicKey();
+
+		assertTrue(kvServerTest == kvClientTest);
+	}
+
+	@Test
+	public void serverInterceptTest() {
 		Exception ex = null;
+		// Public key intercepted, try decryption using that key (private keys are hidden)
+		PublicKey kvServerIntercepted = kvServer1.getClientPublicKey();
+		// Convert string to bytes
+		byte[] test_bytes = test_phrase.getBytes();
+		// Encrypt bytes
+		byte[] encrypted = kvClient1.encrypt(test_bytes, kvServerIntercepted);
+		// Try decrypting using a random private key
+		PrivateKey wrong_key = kvClient5.getClientPrivateKey();
 
 		try {
-			kvServer4.close();
+			byte[] decrypted = kvClient1.decrypt(encrypted, wrong_key);
 		} catch(Exception e) {
 			ex = e;
 		}
-		// assert no error, metadata updates, etc.
-		assertTrue(ex == null);
-	}
-
-	@Test
-	public void testFailureDetection() {
-		Exception ex = null;
-		// KVServerConnection.emptyReceived is a private field, so we can just assume it is 2 for this test
-		
-		// Since kvserver4 was previously closed, we can treat it as our "failed" server
-		try {
-			ECSClient client;
-			testSocket = ECSServerSocket.accept();
-            
-            String tempName = Integer.toString(testSocket.getPort());
-            KVServerConnection connection = new KVServerConnection(testSocket, client, tempName);
-
-			new Thread(connection).start();
-
-		} catch (IOException e) {
-			ex = e;
-		}
-		// The server connection should have failed
+		// The decryption will fail
 		assertTrue(ex != null);
 	}
 
 	@Test
-	public void testRecovery() {
-		// With server 4 closed, 5 and 6 should still have identical values
-		Exception ex = null;
-		Treemap meta5;
-		Treemap meta6;
-		
-		try {
-			meta5 = kvServer5.getMetaData();
-			meta6 = kvServer6.getMetaData();
-		} catch (Exception e) {
-			ex = e;
-		}
+	public void BytesTest() {
+		// For reference, this string is 260 characters --> 260 bytes
+		String longString = "This is a testing string. This is a testing string. This is a testing string. This is a testing string. This is a testing string. This is a testing string. This is a testing string. This is a testing string. This is a testing string. This is a testing string. ";
+		// Convert to bytes
+		byte[] longBytes = longString.getBytes();
+		// Run encryption and decryption
+		byte[] encrypt = kvClient1.encrypt(longBytes, publicKey);
+		byte[] decrypt = kvClient1.decrypt(encrypt, privateKey);
 
-		// no error occured and metadata are equal
-		assertTrue(ex == null);
-		assertTrue(meta5 == meta6);
+		assertTrue(encrypt.length > 245);
+		assertTrue(decrypt.length > 245);
 	}
 
 	@Test
-	public void testKeyRangeRead() {
-		// servers 1-3 constitute a coordinator and two replicas, so the complete keyRangeRead
-		// should go from the start of 1 to the end of 3
-		Exception ex;
-
-		String complete;
-		String test;
-
-		try {
-			String range1 = kvServer1.getKeyrange();
-			String range3 = kvServer3.getKeyrange();
-
-			String[] server1 = range1.split(",");
-			String start = server1[0];
-
-			String[] server3 = range3.split(",");
-			String end = server3[1];
-
-			complete = start + "," + end;
-
-			String keyrangeread = kvServer1.getKeyrangeRead();
-			String[] temp = keyrangeread.split(",");
-			test = temp[0] + "," + temp[1];
-		} catch (Exception e) {
-			ex = e;
-		}
-
-		// no errors occured and key range is complete
-		assertTrue(ex == null);
-		assertTrue(complete == test);
-	}
-
-	@Test
-	public void testCoordToReplicaConnect() {
-		// Servers 5 and 6 are a coordinator replica pair, so we test their connection
-
-		KVMessage response = null;
-		Exception ex = null;
-		
-		try {
-			kvServer5.connectToServer("localhost", 7557);
-		} catch (Exception e) {
-			ex = e;
-		}
-		assertTrue(ex == null);
-	}
-
-	@Test
-	public void testReplica() {
-		// Testing if server x and y have the same KV pair - currently testing 1 and 2
-		Exception ex = null;
-		Treemap meta1;
-		Treemap meta2;
-		
-		String val1;
-		String val2;
-
-		try {
-			meta1 = kvServer1.getMetaData();
-			meta2 = kvServer2.getMetaData();
-
-			val1 = kvServer1.getKV(key);
-			val2 = kvServer2.getKV(key);
-		} catch (Exception e) {
-			ex = e;
-		}
-
-		// no error occured and metadata and KV are equal
-		assertTrue(ex == null);
-		assertTrue(meta1 == meta2);
-		assertTrue(val1 == val2);
+	public void reconnectTest() {
+		kvClient1.disconnect();
+		// Delay for 1 second
+		TimeUnit.SECONDS.sleep(1);
+		// Reconnect server
+		kvClient1.connect();
+		// Check serverPublicKey value
+		PublicKey testKey = kvClient1.getServerPublicKey();
+		// Get server's public key from server directly
+		PublicKey serverKey = kvServer1.getServerPublicKey();
+		assertTrue(testKey == serverKey);
 	}
 }
